a8748324e01d4b05d129b0f7e005a83accd535b9
Jeffrey Sham <jsham@mindgrub.com>
2015-07-31 22:58:25 +0800

    got basic functionality working

--- background.js
+++ background.js
@@ -3,14 +3,14 @@ function setBackgroundImage() {
 	var hours = d.getHours(); //(goes from 0-23) 
 	var img = new Image();
 	
-	/*if (hours >= 6 && hours < 18) {
+	if (hours >= 6 && hours < 18) {
 		//Set background to sunny
 		//Image Credit: Unsplash - Aaron Burden
 		
 		var imgURL = "flower.jpg";
 		img.src = imgURL; 
 		
-		document.getElementsByClassName("bg").style.backgroundImage = imgURL;
+		document.getElementById("img").src = imgURL;
 		 
 	} else {
 		//Set background to night time
@@ -18,6 +18,9 @@ function setBackgroundImage() {
 		var imgURL = "nighttime.jpg";
 		img.src = imgURL; 
 		
-		document.getElementsByClassName("bg").style.backgroundImage = imgURL;
-	}*/
+		document.getElementById("img").src = imgURL;
+		document.getElementById("summary_content").style.color = "#ffffff";
+		document.getElementById("contact_content").style.color = "#ffffff";
+		document.getElementById("column_content").style.color = "#ffffff";
+	}
 }
